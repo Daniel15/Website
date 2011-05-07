01bb46578dc3f18725dfc7dace48450e0e34aeb1
Daniel Lo Nigro <daniel@dan.cx>
2011-05-07 10:13:12 +0000

    Missing files
    git-svn-id: http://vps.dan.cx/svn/daniel15/trunk@42 1220d374-51d1-4dde-b793-d358c6c9dc2f

---
+++ application/classes/controller/blogsidebar.php
@@ -0,0 +1,18 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+/**
+ * Controller for handling blog sidebar rendering
+ * @author Daniel15 <daniel at dan.cx>
+ */
+class Controller_BlogSidebar extends Controller
+{
+	public function action_index()
+	{
+		$sidebar = View::factory('blog/sidebar/index')
+			->set('years', Model_Blog_Post::get_month_counts())
+			->set('month_names', Date::months(Date::MONTHS_LONG))
+			->set('categories', ORM::factory('Blog_Category')->order_by('title')->find_all());
+		$this->response->body($sidebar);
+	}
+}
+?> 
---
+++ application/config/site.php
@@ -0,0 +1,7 @@
+<?php return array (
+  'enableCompression' => false,
+  'latestJS' => 'combined/2011/05/04_1329.js',
+  'latestCSS' => 'combined/2011/05/04_1329.css',
+  'latestBlogJS' => 'combined/2011/04/29_1625_blog.js',
+  'latestBlogCSS' => 'combined/2011/04/29_1625_blog.css',
+); ?>
