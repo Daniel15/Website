3209c5d896e5f1dfb11bffe9e7c5d6216f897b5e
Daniel15 <daniel@dan.cx>
2012-02-26 22:20:30 +1100

    More LESS conversions

--- tools/generate-sprites.bat
+++ tools/generate-sprites.bat
@@ -1,2 +1,2 @@
 @echo off
-cmd /c "cd C:\apps\smartsprites-0.2.8 && smartsprites.cmd C:\xampp\htdocs\d15.biz\res\sprites.css --css-file-suffix=-processed"
+cmd /c "cd C:\apps\smartsprites-0.2.8 && smartsprites.cmd C:\xampp\htdocs\d15.biz\res\sprites.less --css-file-suffix=-processed"
