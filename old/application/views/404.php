9b46bcf8f7e033c2a0a5e9bc54255208587f7727
Daniel Lo Nigro <daniel@dan.cx>
2012-12-18 22:20:14 +1100

    Remove code for stuff that has been implemented in .NET

--- old/application/views/404.php
+++
@@ -1,2 +0,0 @@
-<p>File not found :(</p>
-<p>You might want to go <a href="/">to the index</a> and try to find whatever you're looking for :)</p>
--- old/application/views/sidebars/index.php
+++
@@ -1,7 +0,0 @@
-				<h2><a href="socialfeed.htm">What I've Been Doing</a></h2>
-				<ul id="minifeed">
-					<li class="loading">Loading...</li>
-				</ul>
-				<ul>
-					<li><a href="socialfeed.htm">See more...</a></li>
-				</ul>
