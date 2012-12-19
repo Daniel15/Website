6850cd3da1860c483977cc83b92ef15404df57f9
Daniel Lo Nigro <daniel@dan.cx>
2012-12-19 22:49:38 +1100

    Basic ASP.NET forms authentication with passwords in config file (Authentication.config)

---
+++ Daniel15.Web/ViewModels/Account/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Daniel15.Web.ViewModels.Account
+{
+	public class LoginViewModel : ViewModelBase
+	{
+		public string ReturnUrl { get; set; }
+
+		[Required]
+		[Display(Name = "User name")]
+		public string UserName { get; set; }
+
+		[Required]
+		[DataType(DataType.Password)]
+		[Display(Name = "Password")]
+		public string Password { get; set; }
+	}
+}
