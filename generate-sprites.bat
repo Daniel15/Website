e1b767441275905acb700f67a0227f00b83d04a8
Daniel Lo Nigro <daniel@dan.cx>
2013-02-22 22:37:54 +1100

    Add Github sprite for social feed

---
+++ generate-sprites.bat
@@ -0,0 +1,3 @@
+@echo off
+::: TODO: Remove hard-coded path from here!
+cmd /c "cd C:\apps\smartsprites-0.2.8 && smartsprites.cmd "C:\Users\Daniel\Documents\Visual Studio 2010\Projects\dan.cx_dotnet\Daniel15.Web\Content\css\sprites.less" --css-file-suffix=-processed"
--- old/tools/generate-sprites.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-cmd /c "cd C:\apps\smartsprites-0.2.8 && smartsprites.cmd C:\xampp\htdocs\d15.biz\css\sprites.less --css-file-suffix=-processed"
