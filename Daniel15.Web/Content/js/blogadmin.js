249cff2dd91194ed71982a6c912e48d919861154
Daniel Lo Nigro <daniel@dan.cx>
2013-04-14 00:09:12 +1000

    Category search when editing posts

--- Daniel15.Web/Content/js/blogadmin.js
+++ Daniel15.Web/Content/js/blogadmin.js
@@ -8,28 +8,43 @@ var BlogAdmin =
 
 BlogAdmin.Posts = 
 {
-}
+};
 
 BlogAdmin.Posts.Edit = 
 {
 	init: function()
 	{
-		$('title').addEvent('keyup', this.updateSlug);
+		this.categorySearchEl = $('category-search');
+		this.categoriesEl = $('categories');
+		$('Post_Title').addEvent('keyup', this.updateSlug);
+		this.categorySearchEl.addEvent('keyup', this.categorySearch.bind(this));
 	},
 	
 	updateSlug: function()
 	{
-		var slug = $('title').get('value').toLowerCase()
+		var slug = $('Post_Title').get('value').toLowerCase()
 			.replace(/[^A-Za-z0-9\-]/g, '-')
 			.replace(/\s+/g, '-')
 			.replace(/\-+/g, '-');
 		// Make sure slug doesn't end with hyphen
 		if (slug.charAt(slug.length - 1) == '-')
 			slug = slug.substring(0, slug.length - 1);
-		$('slug').set('value', slug);
+		$('Post_Slug').set('value', slug);
+	},
+	
+	categorySearch: function()
+	{
+		var categoryEls = this.categoriesEl.getByTag('li'),
+			searchTerm = this.categorySearchEl.get('value').toLowerCase();
+		for (var i = 0, count = categoryEls.length; i < count; i++)
+		{
+			var categoryEl = categoryEls[i];
+			var containsTerm = categoryEl.get('textContent').toLowerCase().indexOf(searchTerm) !== -1;
+			categoryEl.setStyle('display', containsTerm ? 'block' : 'none');
+		}
 	}
 };
 
 // Manual initialisation is necessary since this loads after the core code
-if (document.body.id == 'blogadmin-posts-edit')
+if (document.body.id == 'admin-blog-edit')
 	BlogAdmin.Posts.Edit.init();
