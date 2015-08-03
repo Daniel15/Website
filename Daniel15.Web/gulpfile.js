5ec24039c227265cf038c38cd22f68e8e628e7a9
Daniel Lo Nigro <daniel@dan.cx>
2015-08-02 20:48:38 -0700

    Build CSS and JS using Gulp

---
+++ Daniel15.Web/gulpfile.js
@@ -0,0 +1,94 @@
+﻿/// <binding Clean='clean' />
+
+var concat = require('gulp-concat'),
+	del = require('del'),
+    gulp = require('gulp'),
+    lazypipe = require('lazypipe'),
+    less = require('gulp-less'),
+    minifyCSS = require('gulp-minify-css'),
+	react = require('gulp-react'),
+    rename = require('gulp-rename'),
+    uglify = require('gulp-uglify'),
+	urlAdjuster = require('gulp-css-url-adjuster'),
+	project = require('./project.json');
+
+var webroot = './' + project.webroot + '/';
+var paths = {
+	webroot: webroot,
+	css: webroot + 'Content/css/',
+	js: webroot + 'Content/js/',
+	concatRoot: webroot + 'cache/'
+};
+
+gulp.task('clean', function(cb) {
+	del([paths.concatRoot + '/*'], cb);
+});
+
+gulp.task('build:css', function() {
+	return gulp
+		.src([paths.css + 'main.less', paths.css + 'blogadmin.less'])
+		.pipe(less())
+		.pipe(urlAdjuster({
+			replace: ['../', '/Content/']
+		}))
+		.pipe(gulp.dest(paths.concatRoot))
+		.pipe(rename({ extname: '.min.css' }))
+		.pipe(minifyCSS())
+		.pipe(gulp.dest(paths.concatRoot));
+});
+
+var buildJS = lazypipe()
+	.pipe(react, { harmony: true })
+	.pipe(gulp.dest, paths.concatRoot)
+	.pipe(uglify, { preserveComments: 'some' })
+	.pipe(rename, { extname: '.min.js' })
+	.pipe(gulp.dest, paths.concatRoot);
+
+gulp.task('build:js:main', function() {
+	return gulp
+		.src([
+			// Framework
+			paths.js + 'framework/core.js',
+			paths.js + 'framework/ajax.js',
+			paths.js + 'framework/dom.js',
+			paths.js + 'framework/events.js',
+
+			// Site scripts
+			paths.js + 'core.js',
+			paths.js + 'site.jsx',
+			paths.js + 'blog.js',
+			paths.js + 'socialfeed.jsx'
+			])
+		.pipe(concat('main.js'))
+		.pipe(buildJS());
+});
+
+gulp.task('build:js:syntaxHighlighter', function() {
+	return gulp
+		.src([
+			paths.js + 'lib/syntaxhighlighter/shCore.js',
+			paths.js + 'lib/syntaxhighlighter/shBrushJScript.js',
+			paths.js + 'lib/syntaxhighlighter/shBrushPhp.js',
+			paths.js + 'lib/syntaxhighlighter/shBrushCSharp.js',
+			paths.js + 'lib/syntaxhighlighter/shBrushXml.js',
+			paths.js + 'lib/syntaxhighlighter/shBrushDelphi.js',
+			paths.js + 'lib/syntaxhighlighter/shBrushPlain.js',
+			paths.js + 'lib/syntaxhighlighter/shBrushCss.js',
+			paths.js + 'syntaxhighlighter.js'
+		])
+		.pipe(concat('syntaxHighlighter.js'))
+		.pipe(buildJS());
+});
+
+gulp.task('build:js:blogadmin', function () {
+	return gulp
+		.src(paths.js + 'blogadmin.js')
+		.pipe(buildJS());
+});
+
+gulp.task('build', [
+	'build:css',
+	'build:js:main',
+	'build:js:syntaxHighlighter',
+	'build:js:blogadmin'
+]);
