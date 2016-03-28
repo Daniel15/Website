89560ce38ce0953b1fab6b8dbcdcaf5e88a0bc1d
Daniel Lo Nigro <daniel@dan.cx>
2016-03-27 22:56:50 -0700

    Use relative path in deploy.bat

--- deploy-staging.bat
+++ deploy-staging.bat
@@ -1,3 +1,3 @@
 @echo off
-gulp -b "C:\Users\danlo\Documents\Visual Studio 2015\Projects\dan.cx\Daniel15.Web" --color --gulpfile "C:\Users\danlo\Documents\Visual Studio 2015\Projects\dan.cx\Daniel15.Web\Gulpfile.js" deploy:staging
+gulp -b ".\Daniel15.Web" --color --gulpfile ".\Daniel15.Web\Gulpfile.js" deploy:staging
 pause
--- deploy.bat
+++ deploy.bat
@@ -1,3 +1,3 @@
 @echo off
-gulp -b "C:\Users\danlo\Documents\Visual Studio 2015\Projects\dan.cx\Daniel15.Web" --color --gulpfile "C:\Users\danlo\Documents\Visual Studio 2015\Projects\dan.cx\Daniel15.Web\Gulpfile.js" deploy:live
+gulp -b ".\Daniel15.Web" --color --gulpfile ".\Daniel15.Web\Gulpfile.js" deploy:live
 pause
