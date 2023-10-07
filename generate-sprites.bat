19a8f55c2d6bd4fd788392b23e6796fb81dea6b9
Daniel Lo Nigro <d@d.sb>
2023-10-06 23:48:46 -0700

    Add Mastodon and Lemmy links

--- generate-sprites.bat
+++ generate-sprites.bat
@@ -1,3 +1,3 @@
 @echo off
 ::: TODO: Remove hard-coded path from here!
-cmd /c "cd C:\apps\smartsprites-0.2.8 && smartsprites.cmd "C:\Users\Daniel\Documents\Visual Studio 2010\Projects\dan.cx_dotnet\Daniel15.Web\Content\css\sprites.less" --css-file-suffix=-processed"
+cmd /c "cd C:\apps\smartsprites-0.2.13 && smartsprites.cmd "C:\src\dan.cx\Daniel15.Web\wwwroot\Content\css\sprites.less" --css-file-suffix=-processed"
