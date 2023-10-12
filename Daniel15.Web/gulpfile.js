166c92c15e61c457e4d5b44fbe8778cc4420382a
Daniel Lo Nigro <d@d.sb>
2023-10-11 22:42:39 -0700

    Bump Gulp to latest version (will eventually be removed) Add Yarn lockfile

--- Daniel15.Web/gulpfile.js
+++ Daniel15.Web/gulpfile.js
@@ -53,7 +53,7 @@ var buildJS = lazypipe()
 	.pipe(rename, { extname: '.min.js' })
 	.pipe(gulp.dest, paths.concatRoot);
 
-gulp.task('build:js:main', function() {
+gulp.task('build:js:main', function(cb) {
 	return gulp
 		.src([
 			// Framework
@@ -68,10 +68,12 @@ gulp.task('build:js:main', function() {
 			paths.js + 'blog.js',
 			])
 		.pipe(concat('main.js'))
-		.pipe(buildJS());
+		.pipe(buildJS())
+		// https://stackoverflow.com/a/40101404
+		.on('end', cb);
 });
 
-gulp.task('build:js:syntaxHighlighter', function() {
+gulp.task('build:js:syntaxHighlighter', function(cb) {
 	return gulp
 		.src([
 			paths.js + 'lib/syntaxhighlighter/shCore.js',
@@ -85,22 +87,16 @@ gulp.task('build:js:syntaxHighlighter', function() {
 			paths.js + 'syntaxhighlighter.js'
 		])
 		.pipe(concat('syntaxHighlighter.js'))
-		.pipe(buildJS());
+		.pipe(buildJS())
+		.on('end', cb);
 });
 
-gulp.task('build:js:blogadmin', function () {
+gulp.task('build:js:blogadmin', function (cb) {
 	return gulp
 		.src(paths.js + 'blogadmin.js')
-		.pipe(buildJS());
+		.pipe(buildJS())
+		.on('end', cb);
 });
-
-gulp.task('build', [
-	'build:css',
-	'build:js:main',
-	'build:js:syntaxHighlighter',
-	'build:js:blogadmin',
-	'build:config'
-]);
 
 gulp.task('build:config', function(cb) {
 	git.revParse({ args: 'HEAD' }, function(err, revision) {
@@ -118,6 +114,14 @@ gulp.task('build:config', function(cb) {
 		});
 	});
 });
+
+gulp.task('build', gulp.series([
+	'build:css',
+	'build:js:main',
+	'build:js:syntaxHighlighter',
+	'build:js:blogadmin',
+	'build:config'
+]));
 
 // TODO: Move this into an MSBuild script, there's no need for it to be in Gulp
 gulp.task('package', function () {
@@ -138,10 +142,10 @@ function deploy(remoteDir) {
 		));
 }
 
-gulp.task('deploy:staging', ['package'], function () {
+gulp.task('deploy:staging', gulp.series(['package']), function () {
 	return deploy('staging');
 });
 
-gulp.task('deploy:live', ['package'], function () {
+gulp.task('deploy:live', gulp.series(['package']), function () {
 	return deploy('live');
 });
